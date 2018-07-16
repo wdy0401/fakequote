6529c432df4da6fd48fe0b41428c648fab6c3645
wdy0401@gmail.com <wdy0401@gmail.com>
2018-07-16 16:28:46 +0800

    fake quote 具体内容伪码

--- fakequote.py
+++ fakequote.py
@@ -34,14 +34,21 @@ import pandas as pd
     
 细节:
     先看old超出new部分有多少
-    超出部分的量通过删掉1/2 压缩1/2进行处理 可以调节压缩删除比例
-    删除压缩区别在于  删除是整段数据 也就是数个bar 压缩是去一个丢一个
-    现在采取不区别两种删除的方式  也就是m个k线里面取n个
+   T 超出部分的量通过删掉1/2 压缩1/2进行处理 可以调节压缩删除比例
+    删除压缩区别在于  删除是整段数据 也就是数个bar 多个bar合成一个bar
+    现在均采取删除的方式  也就是m个k线里面取n个
     arr = np.arange(10)
     np.random.shuffle(arr)
     arr
     
+    先把源数据粒度统一成需求精度
+    
+    成交量需要添加微幅波动
+    集合竞价成交量也需要按添加微幅波动 以免可与日期对应
+    如何将集合竞价数据统一成k线数据 
+    
 '''
+
 class time_map(object):
     # pre: day 1min 6sec 500ms
     def set_pre(self,pre):
@@ -50,7 +57,43 @@ class time_map(object):
 
 
 class stock(object):
-    pass
+    def load_histroy(self):
+        #load every day
+        #merge to one dataframe
+        pass
+    def pre_convert(self):
+        #day to standard bar
+        #deal open close auction
+        pass
+    def time_select(self):
+        #gen random selected k bars from total m bars
+        #k different from 
+        #   exchange    close auction
+        #   pre      tick or 1minbar or etc
+        pass
+    def conbine_bar(self):
+        #conbine bars with time select
+        #get open from pre set info or determined from today ohlc
+        #keep inter bar return(between ori and fake) 
+        pass
+    def hl_limit_adj(self):
+        #get last settlement price
+        #cut price which is higher or lower than limit
+        pass
+    def volume_adj(self):
+        #9:30-10:00 adj
+        #14:50-15:00 adj
+        #ratio=(the openclose bar)/(average of none openclose bar)
+        pass
+    def auction_adj(self):
+        #9:25 for sh sz
+        #15:00 for sz
+        pass
 
 
+a=pd.read_csv("./data/md/20180102/000001_2.bak.csv",parse_dates=True)
+a.index=[pd.Timestamp(str(a['TradingDay'][x])+" "+str(a['UpdateTime'][x])) for x in range(len(a))]
+
+a=pd.read_csv("./data/md/20180102/600000_1.bak.csv",parse_dates=True,encoding="GBK")
+a.index=[pd.Timestamp(str(a['TradingDay'][x])+" "+str(a['UpdateTime'][x])) for x in range(len(a))]
 
