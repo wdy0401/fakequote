a8a9b56089a905def6006295e95a63379581eb2e
wdy0401@gmail.com <wdy0401@gmail.com>
2018-07-19 16:41:53 +0800

    按文件模式进行的策略排名脚本

--- para_cal.py
+++ para_cal.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import math
-import sys
+import os  
 YTD=252#每年交易日数量
 
 '''ind_cal函数接受一个净值序列 一个参考序列 返回收益风险指标
@@ -59,11 +59,22 @@ def ind_cal(nv,bm,name):
     sortino=yret/downvol
     rlist=[yret,eyret,yshp,yic,alpha,beta,maxdrop,yvol,downvol,sortino]
     plist=[str(round(x,4)) for x in rlist]
-    sys.stderr.write("策略名,年化收益率,超额收益率,夏普比率,信息比率,阿尔法,贝塔,最大回撤,年化波动率,下行波动率,索提诺比率")
-    sys.stderr.write(",".join([name]+plist))
-    print(",".join([name]+plist))
-    return rlist
+    print_list=",".join([name]+plist)
+    return print_list
 
-nv=np.array([1,1.33518481,0.5947847,1.56663256,1.07746506,0.99541804,1.09275599,1.23980119,1.08549716,1.17878604,1.43788036,1.84143297,1.71776313,1.64087145,1.84838018,1.87778502,2.12587365,1.8996966,2.35480062,2.35346487])
-bm=np.array([1,1.04650794,0.71964821,1.21264349,1.27265859,1.13142881,1.16854388,1.09086115,1.29109976,1.16364775,1.53104984,1.77451284,1.56222544,1.63383812,1.4862519,1.95264773,1.81769526,1.85748236,2.11899229,1.99498051])
-ind_cal(nv,bm,"Tactic1")
+def load_net(fname):
+    with open(fname) as f:
+        return [float(str.strip(x)) for x in f.readlines()]
+
+usedir="./data"
+with open("tactic_para.csv","w") as f:
+    dirs=os.listdir(usedir)
+    for fn in dirs:
+        if fn[-3:]=="csv":
+            nv=load_net(usedir+"/"+fn)
+            fakebm=[1+0.0001*i for i in range(len(nv))]
+            f.write(ind_cal(nv,fakebm,os.path.split(fn)[1][0:-4])+"\n")
+
+
+    
+    
--- tactic_comparation.py
+++ tactic_comparation.py
@@ -11,7 +11,6 @@ warnings.filterwarnings("ignore")
 paralist=["yret","eyret","yshp","yic","alpha","beta","maxdrop","yvol","downvol","sortino"]
 d=pd.read_csv("tactic_para.csv",header=None,index_col=0,names=paralist)
 
-
 d['rank']=0 
 duni=d[d['yret']>=0]
 dnuni=d[d['yret']<0]
@@ -32,33 +31,16 @@ if len(duni>0):
 if len(dnuni>0):   
    dnuni['rank']=dnuni['yret']
 
-def print_rank(dpara):
+def print_rank(dpara,f):
     if len(dpara>0):
         order=dict(zip(dpara.index,dpara['rank']))
         for rank_value in sorted(order.values(),reverse=True):
             for i in order.keys():#这个循环解决了重复rank_value的问题
                 if order[i]==rank_value:
-                    print(print_rank.ranking,i,round(order[i],4))
+                    f.write(",".join([str(x) for x in [print_rank.ranking,i,round(order[i],4),*list(d.loc[i])]])+"\n")
                     print_rank.ranking=print_rank.ranking+1
-print_rank.ranking=1
-print_rank(duni)
-print_rank(dnuni)
+with open("tactic_rank.csv","w") as f: 
+    print_rank.ranking=1
+    print_rank(duni,f)
+    print_rank(dnuni,f)
 
-
-'''
-	同类策略排名步骤
-	排除收益为负的策略
-	将各个指标进行归一化处理
-	收益率分值不变
-	夏普比率按照公式(e^x-1)⁄(e-1)进行处理
-	最大回撤按公式 (e-e^x)⁄(e-1)进行处理
-	将收益率，夏普，最大回撤分数相加并排名得到同类策略排名结果
-	其他指标作为定性比较参考依据
-	
-    尚未得到策略类别信息 下面方法暂无法实现
-    非同类策略排名步骤
-	将策略进行同类排名
-	选出每类排名前三的策略
-	将选出的策略按夏普比率进行排名得到非同类策略排名结果
-	其他指标所谓定性比较参考依据
-'''
